96ae4eefbb4741655ab00c2b3e775565623b0275
Angelofdeath2 <ashley.ly95@gmail.com>
2019-12-05 17:34:26 +0100

    first commit

---
+++ src/main/java/miage/aly/bowling/App.java
@@ -0,0 +1,13 @@
+package miage.aly.bowling;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
